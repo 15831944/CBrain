93080ba969eb287c658d8196996e72bed0a84e15
TischlerWilly <oliver.schuft@gmx.de>
2018-07-25 20:41:54 +0200

    modul lager aus ids umgestellt

--- _todo.h
+++ _todo.h
@@ -7,9 +7,7 @@ soll als Template zum Anlegen von neuen projektbezogenen Materiallisten dienen
 
 
 ->Lager->Druckerdialog erweitern um Angabe zum Lieferanten
-->Lager->Vorgang als id speichern nicht als text
-    ->Tabelle mit Klartext zur ID anlegen
-->Lager->Kommission als id speichern nicht als text
+
 
 -->Im-Export-Dialog
 
